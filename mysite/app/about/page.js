8fec745e40e74ab17053c86da8e1f14a6508f502
John Murray <johnmurray@fastmail.com>
2022-11-16 09:46:37 -0500

    adds WIP resume download from about page

--- mysite/app/about/page.js
+++ mysite/app/about/page.js
@@ -1,3 +1,17 @@
+"use client";
+import { Anchor } from "grommet";
+import { DocumentDownload } from "grommet-icons";
+
 export default function Page() {
-  return <h1>About...</h1>;
+  return (
+    <>
+      <h1>About...</h1>
+      <DocumentDownload />
+      <Anchor
+        href="John_Murray_Resume.pdf"
+        label="Download my resume."
+        download
+      />
+    </>
+  );
 }
