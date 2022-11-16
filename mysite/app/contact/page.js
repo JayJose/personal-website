1e673454389608ba082e5dada22af3541e0e8a87
John Murray <johnmurray@fastmail.com>
2022-11-16 11:00:32 -0500

    add WIP contact

--- mysite/app/contact/page.js
+++ mysite/app/contact/page.js
@@ -1,3 +1,25 @@
+"use client";
+
+import { Anchor } from "grommet";
+import { Phone, Mail } from "grommet-icons";
+
 export default function Page() {
-  return <h1>Contact...</h1>;
+  return (
+    <div className="contact">
+      <h1>Get in touch!</h1>
+      <div className="contact--item">
+        <Phone />
+        <h2>(205) 218-7236</h2>
+      </div>
+      <div className="contact--item">
+        <Mail />
+        <h2>
+          <Anchor
+            href="mailto:johnmurray@fastmail.com"
+            label="johnmurray@fastmail.com"
+          ></Anchor>
+        </h2>
+      </div>
+    </div>
+  );
 }
