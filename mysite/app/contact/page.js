6dc5af46e35117893daa41d40228ab4348d9cbea
John Murray <johnmurray@fastmail.com>
2022-11-19 10:54:31 -0500

    jazz theme

--- mysite/app/contact/page.js
+++ mysite/app/contact/page.js
@@ -4,17 +4,19 @@ import { Anchor } from "grommet";
 import { Phone, Mail } from "grommet-icons";
 
 export default function Page() {
+  const iconColor = "black";
   return (
     <div className="contact">
       <h1>Get in touch!</h1>
       <div className="contact--item">
-        <Phone />
+        <Phone color={iconColor} />
         <h2>(205) 218-7236</h2>
       </div>
       <div className="contact--item">
-        <Mail />
+        <Mail color={iconColor} />
         <h2>
           <Anchor
+            color={"black"}
             href="mailto:johnmurray@fastmail.com"
             label="johnmurray@fastmail.com"
           ></Anchor>
--- mysite/app/work/page.js
+++ mysite/app/work/page.js
@@ -5,13 +5,17 @@ import { DocumentDownload } from "grommet-icons";
 export default function Page() {
   return (
     <>
-      <h1>About...</h1>
-      <DocumentDownload />
+      <h1>Work</h1>
+      <DocumentDownload color={"black"} />
       <Anchor
+        color={"black"}
         href="John_Murray_Resume.pdf"
         label="Download my resume."
         download
       />
+      <p>Experience</p>
+      <p>Certifications & Skills</p>
+      <p>Publications</p>
     </>
   );
 }
